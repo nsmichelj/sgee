abb50b336e944b5d57abc5bc8e8e38643ccf74ae
nsmichelj <nunezsalmeronm@gmail.com>
2026-03-02 10:22:49 -0400

    feat(pages): implement dashboard page

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,39 @@
+import { Container } from "@/components/container";
+import { AppSidebar } from "@/components/dashboard/sidebar";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import { QueryProvider } from "@/providers/query-provider";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Unidad Educativa Pedro Luis Cedeño",
+  description:
+    "Sistema de Gestión Extracurricular Escolar de la Unidad Educativa Pedro Luis Cedeño.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <SidebarProvider>
+      <AppSidebar />
+      <SidebarInset>
+        <header className="flex h-16 shrink-0 items-center gap-2 bg-background sticky top-0 border-b border-border z-50">
+          <div className="flex items-center gap-2 px-4">
+            <SidebarTrigger className="-ml-1" />
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col gap-4">
+          <QueryProvider>
+            <Container className="py-12">{children}</Container>
+          </QueryProvider>
+        </div>
+      </SidebarInset>
+    </SidebarProvider>
+  );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,18 @@
+import {
+  PanelDescription,
+  PanelHeader,
+  PanelTitle,
+} from "@/components/dashboard/panel";
+
+export default function Page() {
+  return (
+    <div>
+      <PanelHeader>
+        <PanelTitle>Dashboard</PanelTitle>
+        <PanelDescription>
+          Vienvenido al panel de control del SGEE Pedro Luis Cedeño
+        </PanelDescription>
+      </PanelHeader>
+    </div>
+  );
+}
