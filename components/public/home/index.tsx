40f4294000073b1c4b1b1e78b293c51fb6317e2d
nsmichelj <nunezsalmeronm@gmail.com>
2026-03-06 21:03:52 -0400

    feat(ui): add RecentNews component to home exports

--- components/public/home/index.tsx
+++ components/public/home/index.tsx
@@ -3,5 +3,6 @@ import { Gallery } from "./gallery";
 import { Hero } from "./hero";
 import { Leadership } from "./leadership";
 import { MisionVision } from "./mision-vision";
+import { RecentNews } from "./recent-news";
 
-export { Contact, Gallery, Hero, Leadership, MisionVision };
+export { Contact, Gallery, Hero, Leadership, MisionVision, RecentNews };
