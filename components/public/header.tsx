8e2de5c40603c19e9219a1711092ec801453c465
nsmichelj <nunezsalmeronm@gmail.com>
2026-03-28 13:25:34 -0400

    feat(page): Home redesign

--- components/public/header.tsx
+++ components/public/header.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { Container } from "../container";
 import { Button } from "../ui/button";
@@ -5,17 +6,39 @@ import { Button } from "../ui/button";
 export function Header() {
   return (
     <header className="sticky top-0 left-0 z-50">
-      <nav className="relative bg-background w-full z-20 border-b border-border">
-        <Container className="flex flex-wrap items-center justify-between py-4">
-          <Link href="/">
-            <span className="text-xl font-semibold">
+      <div className="mx-auto bg-background">
+        <Container className="flex justify-between items-center py-4 w-full">
+          <Link href="/" className="flex gap-4 items-center">
+            <Image
+              src="/institutional_logo.png"
+              width={100}
+              height={109}
+              alt="Logo Insitucional Unidad Educativa Pedro Luis Cedeño"
+              className="max-w-15 h-auto mx-auto"
+            />
+            <span className="text-xl md:text-2xl font-semibold">
               Unidad Educativa Pedro Luis Cedeño
             </span>
           </Link>
-          <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
+          <Image
+            src="/institutional_banner.jpg"
+            width={500}
+            height={200}
+            alt="Logo Insitucional Unidad Educativa Pedro Luis Cedeño"
+            className="max-w-52 h-auto ms-auto"
+          />
+        </Container>
+      </div>
+      <nav className="relative bg-primary w-full z-20 border-b border-primary">
+        <Container className="flex justify-between items-center py-2 w-full">
+          <div className="w-full">
+            <ul className="flex gap-4">
               <li>
-                <Button variant="link" className="text-lg" asChild>
+                <Button
+                  variant="link"
+                  className="text-md text-white p-0 uppercase"
+                  asChild
+                >
                   <Link href="/school_news">Noticias</Link>
                 </Button>
               </li>
--- components/public/home/hero.tsx
+++ components/public/home/hero.tsx
@@ -1,36 +1,38 @@
-import Image from "next/image";
-
 export function Hero() {
   return (
-    <section className="relative h-150 flex items-center justify-center overflow-hidden">
+    <section
+      className="relative h-80 md:h-140 lg:h-180 bg-cover bg-center"
+      style={{
+        backgroundImage: 'url("/school_banner.jpeg")',
+      }}
+    >
       <div
-        className="absolute inset-0 bg-cover bg-center"
+        className="absolute bottom-0 left-0 w-full"
         style={{
-          backgroundImage: 'url("/school_banner.jpeg")',
+          overflow: "hidden",
         }}
       >
-        <div className="absolute inset-0 bg-black/70"></div>
-      </div>
-
-      <div className="relative z-10 text-center px-4 sm:px-6 lg:px-8 max-w-4xl mx-auto space-y-4">
-        <div className="size-36 flex items-center justify-center bg-white border-gray-500 border-4 mx-auto rounded-full p-5">
-          <Image
-            src="/institutional_logo.png"
-            width={100}
-            height={100}
-            alt="Logo Insitucional Unidad Educativa Pedro Luis Cedeño"
-            className="w-auto h-auto"
+        <svg
+          preserveAspectRatio="none"
+          viewBox="0 0 1200 120"
+          xmlns="http://www.w3.org/2000/svg"
+          style={{
+            fill: "var(--secondary)",
+            width: "100%",
+            transform: "rotate(180deg)",
+          }}
+          className="h-15 md:h-30"
+        >
+          <path
+            d="M0 0v46.29c47.79 22.2 103.59 32.17 158 28 70.36-5.37 136.33-33.31 206.8-37.5 73.84-4.36 147.54 16.88 218.2 35.26 69.27 18 138.3 24.88 209.4 13.08 36.15-6 69.85-17.84 104.45-29.34C989.49 25 1113-14.29 1200 52.47V0z"
+            opacity=".25"
           />
-        </div>
-        <h1>
-          <span className="text-3xl md:text-4xl font-bold text-white">
-            Unidad Educativa
-          </span>
-          <br />
-          <span className="text-4xl md:text-5xl lg:text-6xl font-bold text-balance text-white">
-            Pedro Luis Cedeño
-          </span>
-        </h1>
+          <path
+            d="M0 0v15.81c13 21.11 27.64 41.05 47.69 56.24C99.41 111.27 165 111 224.58 91.58c31.15-10.15 60.09-26.07 89.67-39.8 40.92-19 84.73-46 130.83-49.67 36.26-2.85 70.9 9.42 98.6 31.56 31.77 25.39 62.32 62 103.63 73 40.44 10.79 81.35-6.69 119.13-24.28s75.16-39 116.92-43.05c59.73-5.85 113.28 22.88 168.9 38.84 30.2 8.66 59 6.17 87.09-7.5 22.43-10.89 48-26.93 60.65-49.24V0z"
+            opacity=".5"
+          />
+          <path d="M0 0v5.63C149.93 59 314.09 71.32 475.83 42.57c43-7.64 84.23-20.12 127.61-26.46 59-8.63 112.48 12.24 165.56 35.4C827.93 77.22 886 95.24 951.2 90c86.53-7 172.46-45.71 248.8-84.81V0z" />
+        </svg>
       </div>
     </section>
   );
